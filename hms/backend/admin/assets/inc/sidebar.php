47702615b2b2678f7f158314fa48baf5ec81e645
Kodand Ram <movva.k@northeastern.edu>
2024-04-24 15:54:17 -0400

    push

---
+++ hms/backend/admin/assets/inc/sidebar.php
@@ -0,0 +1,189 @@
+<div class="left-side-menu">
+
+                <div class="slimscroll-menu">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+
+                        <ul class="metismenu" id="side-menu">
+
+                            <li class="menu-title">Navigation</li>
+
+                            <li>
+                                <a href="his_admin_dashboard.php">
+                                    <i class="fe-airplay"></i>
+                                    <span> Dashboard </span>
+                                </a>
+                                
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fab fa-accessible-icon "></i>
+                                    <span> Patients </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_register_patient.php">Register Patient</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_patients.php">View Patients</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_patient.php">Manage Patients</a>
+                                    </li>
+                                    <hr>
+
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-doctor"></i>
+                                    <span> Employees </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_employee.php">Add Employee</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_employee.php">View Employees</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_employee.php">Manage Employees</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_assaign_dept.php">Assign Department</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_transfer_employee.php">Transfer Employee</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-cash-multiple "></i>
+                                    <span> Accounting </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_acc.payable.php">Add Acc. Payable</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_acc_payable.php">Manage Acc. Payable</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_add_acc_receivable.php">Add Acc. Receivable</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_acc_receivable.php">Manage Acc. Receivable</a>
+                                    </li>
+                                    <hr>
+                                    
+                                    
+                                </ul>
+                            </li>
+                
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fe-share"></i>
+                                    <span> Reporting </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_inpatient_records.php">InPatient Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_outpatient_records.php">OutPatient Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_employee_records.php">Employee Records</a>
+                                    </li>
+
+                                    <li>
+                                        <a href="his_admin_accounting_records.php">Accounting Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_medical_records.php">Medical Records</a>
+                                    </li>
+                                    
+                                </ul>
+                            </li>
+
+
+                                    
+                       
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-flask"></i>
+                                    <span> Laboratory </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_patient_lab_test.php">Patient Lab Tests</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_patient_lab_result.php">Patient Lab Results</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_patient_lab_vitals.php">Patient Vitals</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_employee_lab_vitals.php">Employee Vitals</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_lab_report.php">Lab Reports</a>
+                                    </li>
+                                    <hr>
+
+                                </ul>
+                            </li>
+
+
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-cash-refund "></i>
+                                    <span> Payrolls </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_payroll.php">Add Payroll</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_payrolls.php">Manage Payrolls</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_generate_payrolls.php">Generate Payrolls</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+            
+                                    
+                                </ul>
+                            </li>
+
+
+                        </ul>
+
+                    </div>
+                    <!-- End Sidebar -->
+
+                    <div class="clearfix"></div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
---
+++ hms/backend/admin/his_admin_dashboard.php
@@ -0,0 +1,165 @@
+<?php
+  session_start();
+  include('assets/inc/config.php');
+  include('assets/inc/checklogin.php');
+  check_login();
+  $aid=$_SESSION['ad_id'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+    
+    <!--Head Code-->
+    <?php include("assets/inc/head.php");?>
+
+    <body>
+
+        <!-- Begin page -->
+        <div id="wrapper">
+
+            <!-- Topbar Start -->
+            <?php include('assets/inc/nav.php');?>
+            <!-- end Topbar -->
+
+            <!-- ========== Left Sidebar Start ========== -->
+            <?php include('assets/inc/sidebar.php');?>
+            <!-- Left Sidebar End -->
+
+            <!-- ============================================================== -->
+            <!-- Start Page Content here -->
+            <!-- ============================================================== -->
+
+            <div class="content-page">
+                <div class="content">
+
+                    <!-- Start Content-->
+                    <div class="container-fluid">
+                        
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box">
+                                    <h4 class="page-title">Hospital Management System Dashboard</h4>
+                                </div>
+                            </div>
+                        </div>     
+                        <!-- end page title -->
+
+                        <div class="row">
+                            <!--Start OutPatients-->
+                            <div class="col-md-6 col-xl-4">
+                                <div class="widget-rounded-circle card-box">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <div class="avatar-lg rounded-circle bg-soft-primary border-primary border">
+                                                <i class="fab fa-accessible-icon  font-22 avatar-title text-primary"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="text-right">
+                                                <?php
+                                                    $result ="SELECT count(*) FROM his_patients WHERE pat_type = 'OutPatient' ";
+                                                    $stmt = $mysqli->prepare($result);
+                                                    $stmt->execute();
+                                                    $stmt->bind_result($outpatient);
+                                                    $stmt->fetch();
+                                                    $stmt->close();
+                                                ?>
+                                                <h3 class="text-dark mt-1"><span data-plugin="counterup"><?php echo $outpatient;?></span></h3>
+                                                <p class="text-muted mb-1 text-truncate">Out Patients</p>
+                                            </div>
+                                        </div>
+                                    </div> <!-- end row-->
+                                </div> <!-- end widget-rounded-circle-->
+                            </div> <!-- end col-->
+                            <!--End Out Patients-->
+
+                            <!--Start InPatients-->
+                            <div class="col-md-6 col-xl-4">
+                                <div class="widget-rounded-circle card-box">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <div class="avatar-lg rounded-circle bg-soft-primary border-primary border">
+                                                <i class="mdi mdi-hotel   font-22 avatar-title text-primary"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="text-right">
+                                                <?php
+                                                    $result ="SELECT count(*) FROM his_patients WHERE pat_type = 'InPatient' ";
+                                                    $stmt = $mysqli->prepare($result);
+                                                    $stmt->execute();
+                                                    $stmt->bind_result($inpatient);
+                                                    $stmt->fetch();
+                                                    $stmt->close();
+                                                ?>
+                                                <h3 class="text-dark mt-1"><span data-plugin="counterup"><?php echo $inpatient;?></span></h3>
+                                                <p class="text-muted mb-1 text-truncate">In Patients</p>
+                                            </div>
+                                        </div>
+                                    </div> <!-- end row-->
+                                </div> <!-- end widget-rounded-circle-->
+                            </div> <!-- end col-->
+                            <!--End InPatients-->
+
+                            <!--Start Employees-->
+                            <div class="col-md-6 col-xl-4">
+                                <div class="widget-rounded-circle card-box">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <div class="avatar-lg rounded-circle bg-soft-primary border-primary border">
+                                                <i class="mdi mdi-doctor font-22 avatar-title text-primary"></i>
+                                            </div>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="text-right">
+                                                <?php
+                                                    $result ="SELECT count(*) FROM his_docs ";
+                                                    $stmt = $mysqli->prepare($result);
+                                                    $stmt->execute();
+                                                    $stmt->bind_result($doc);
+                                                    $stmt->fetch();
+                                                    $stmt->close();
+                                                ?>
+                                                <h3 class="text-dark mt-1"><span data-plugin="counterup"><?php echo $doc;?></span></h3>
+                                                <p class="text-muted mb-1 text-truncate">Hospital Employees</p>
+                                            </div>
+                                        </div>
+                                    </div> <!-- end row-->
+                                </div> <!-- end widget-rounded-circle-->
+                            </div> <!-- end col-->
+                            <!--End Employees-->
+                        </div>
+
+                    </div> <!-- container -->
+
+                </div> <!-- content -->
+
+                <!-- Footer Start -->
+                <?php include('assets/inc/footer.php');?>
+                <!-- end Footer -->
+
+            </div>
+
+            <!-- ============================================================== -->
+            <!-- End Page content -->
+            <!-- ============================================================== -->
+
+        </div>
+        <!-- END wrapper -->
+
+        <!-- Vendor js -->
+        <script src="assets/js/vendor.min.js"></script>
+
+        <!-- Plugins js-->
+        <script src="assets/libs/jquery-knob/jquery.knob.min.js"></script>
+        <script src="assets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
+
+        <!-- Dashboard init js-->
+        <script src="assets/js/pages/dashboard-1.init.js"></script>
+
+        <!-- App js-->
+        <script src="assets/js/app.min.js"></script>
+        
+    </body>
+
+</html>
